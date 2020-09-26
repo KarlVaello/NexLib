6ed7e2abc41fad59eae24d99c5a80544199f3a5b
carlos <karlvaello@gmail.com>
2020-09-27 01:57:29 +0200

    Added soft reset cmd
    Added soft reset command to NexComm class to reset display by software

--- NexComm.cpp
+++ NexComm.cpp
@@ -1,3 +1,24 @@
+/*!
+@file    NexComm.cpp
+@brief   CPP file of the class NexComm. This class is used for comunicate to Nextion display
+@author  Carlos Vaello (karlvaello)
+@date	 2020-09-26
+
+This is part of the NexLib used to fully control a Nextion display over serial port.
+
+The MIT License (MIT)
+
+Copyright (c) 2020 Carlos Vaello
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 #include <stdio.h>
 #include <iostream>
 #include <cstring>
@@ -5,14 +26,56 @@
 #include "NexComm.h"
 #include "serialib.h"
 
+// Serial object
+serialib serial;
+
+
+NexComm::NexComm(char* serialPort, int bd)
+{
+	serialPortName = serialPort;
+	baudrate = bd;
+	StartComm(serialPortName, baudrate);
+}
+
+NexComm::~NexComm()
+{
+}
+
+unsigned char NexComm::StartComm(char* serialPort, int bd)
+{
+	printf("Start Connection %s@%d\n", serialPort, bd);
+	char errorOpening = serial.openDevice(serialPort, bd);
+	// If connection fails, return the error code otherwise, display a success message
+	if (errorOpening != 1)
+	{
+		printf("Fail to connect to %s @ %d\n", serialPort, bd);
+		return 0;
+	}
+	else {
+		printf("Successful connection to %s @ %d\n", serialPort, bd);
+		return 1;
+	}
+}
 
 void NexComm::SendCmd(char* cmd, bool tail, bool header)
 {
     serial.writeString(cmd);
     if (tail)
     {
-        
+		CmdTail();
     }
+}
+
+void NexComm::CmdTail()
+{
+	serial.writeChar('ÿ');
+	serial.writeChar('ÿ');
+	serial.writeChar('ÿ');
+}
+
+void NexComm::SoftReset()
+{
+	SendCmd("rest", true, false);
 }
 
 bool NexComm::Connect()
--- NexComm.h
+++ NexComm.h
@@ -1,3 +1,23 @@
+/*!
+@file    NexComm.h
+@brief   Header file of the class NexComm. This class is used for comunicate to Nextion display
+@author  Carlos Vaello (karlvaello)
+@date	 2020-09-26
+
+This is part of the NexLib used to fully control a Nextion display over serial port.
+
+The MIT License (MIT)
+
+Copyright (c) 2020 Carlos Vaello
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
 #pragma once
 #include <stdio.h>
 
@@ -7,10 +27,38 @@ class NexComm
 {
 public:
 
-	/**
-		@brief This function is uses to test display connection
+	char* serialPortName;
+	int baudrate;
 
-		@details  Send a connection test command ("connect") + CmdTail()
+	/**
+		@brief NexComm constructor.
+
+		@param serialPort	serial port where display is connected
+		@param bd			serial communication baudrate
+	*/
+	NexComm(char* serialPort, int bd);
+
+	/**
+		@brief NexComm destructor.
+	*/
+	~NexComm();
+
+	/**
+	@brief This function is uses to star display serial connection
+
+	@details  Send a connection test command ("connect") + CmdTail()
+
+	@param	serialPort		serial port name to open
+	@param	bd				serial communication baudrate
+	@return result of serial comm start (1=OK, other = fail).
+	*/
+	unsigned char StartComm(char* serialPort, int bd);
+
+
+	/**
+		@brief Used to send a standart Nextion command
+
+		@details send a command over serial 
 
 		@param	cmd		String cmd
 		@param	tail	add command termination add the endd of the string cmd
@@ -18,6 +66,16 @@ public:
 		@return result of connection test.
 	*/
 	void SendCmd(char* cmd, bool tail, bool header);
+
+	/**
+		@brief nextion command termination
+
+		@details send "ÿÿÿ" over serial port
+	*/
+	void CmdTail();
+
+	void SoftReset();
+
 
 	/**
 		@brief This function is uses to test display connection
